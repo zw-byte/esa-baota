09c4901a899412ebcbbd8bfff7c29d054387702b
OneDay AI 代码助手 <oneday_bot@alibaba-inc.com>
2026-01-14 13:57:42 +0800

    1BMTJw47_ajngSI3_update-hero-logos_更新 Hero 区域 Logo @ 马颖丽

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,10 +14,7 @@ import React from 'react';
             <div className="mb-10 flex flex-wrap justify-center items-center gap-6 select-none opacity-90">
               {/* 阿里云 Logo */}
               <div className="flex items-center gap-2.5">
-                <svg viewBox="0 0 1024 1024" className="w-8 h-8 md:w-9 md:h-9" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M213.333333 213.333333h170.666667v128H298.666667v341.333334h85.333333v128H213.333333V213.333333z m597.333334 0v597.333334h-170.666667v-128h85.333333V341.333333h-85.333333V213.333333h170.666667z" fill="#FF6A00" />
-                  <path d="M469.333333 469.333333h85.333334v85.333334h-85.333334z" fill="#FF6A00" />
-                </svg>
+                <img src="https://img.alicdn.com/imgextra/i4/O1CN019XW99I1R2Z2Z2Z2Z2_!!6000000002052-2-tps-900-900.png" alt="阿里云" className="w-8 h-8 md:w-9 md:h-9" />
                 <span className="text-xl md:text-2xl font-bold text-slate-700 tracking-tight">阿里云</span>
               </div>
 
@@ -25,12 +22,7 @@ import React from 'react';
 
               {/* 宝塔 Logo */}
               <div className="flex items-center gap-2.5">
-                <svg viewBox="0 0 1024 1024" className="w-7 h-7 md:w-8 md:h-8" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M512 100L128 240V512C128 760 512 924 512 924C512 924 896 760 896 512V240L512 100Z" fill="#20a53a"/>
-                  <path d="M512 240L700 320V380H324V320L512 240Z" fill="white" opacity="0.9"/>
-                  <path d="M340 420H684V480H340V420Z" fill="white" opacity="0.9"/>
-                  <path d="M360 520H664V580H360V520Z" fill="white" opacity="0.9"/>
-                </svg>
+                <img src="https://img.alicdn.com/imgextra/i2/O1CN01u35iJt1uYJ0Q2Z2Z2_!!6000000006052-2-tps-460-460.png" alt="宝塔面板" className="w-7 h-7 md:w-8 md:h-8" />
                 <span className="text-xl md:text-2xl font-bold text-slate-700 tracking-tight">宝塔面板</span>
               </div>
             </div>
